ad958c898c03fe3adad8d6c56847252af19e2829
rkskek8484-cell <rkskek8484@gmail.com>
2026-08-06 10:39:27 +0900

    feat: 유저 상태, 로그아웃

--- src/app/insert/page.js
+++ src/app/insert/page.js
@@ -58,6 +58,11 @@ export default function Insert() {
 
     setFormData({ ...formData, [name]: value });
   };
+  const handleAuthChange = e => {
+    const { name, value } = e.target;
+
+    setAuthform(prev => ({ ...prev, [name]: value }));
+  };
 
   const handleFileChange = e => {
     setThumbnail(e.target.files[0]);
@@ -72,6 +77,50 @@ export default function Insert() {
       // Handle success
       console.log('파일 업로드 성공');
     }
+  }
+
+  //로그인 진행
+  const handleLogin = async e => {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signInWithPassword(authForm);
+    if (error) {
+      alert('로그인 실패', error.message);
+    } else {
+      alert('로그인 성공');
+      router.refresh();
+    }
+  };
+
+  if (!user) {
+    return (
+      <>
+        <div className='about_content shadow'>
+          <h2>관리자 로그인</h2>
+          <div className='contact_form'>
+            <form onSubmit={handleLogin}>
+              <p className='field'>
+                <label htmlFor='email'>이메일</label>
+                <input type='email' id='email' name='email' placeholder='이메일' required onChange={handleAuthChange} />
+              </p>
+              <p className='field'>
+                <label htmlFor='password'>비밀번호</label>
+                <input
+                  type='password'
+                  id='password'
+                  name='password'
+                  placeholder='비밀번호'
+                  required
+                  onChange={handleAuthChange}
+                />
+              </p>
+              <p className='submit'>
+                <input type='submit' className='primary-btn' value='로그인' />
+              </p>
+            </form>
+          </div>
+        </div>
+      </>
+    );
   }
 
   return (
